38da96ada383669625409802f39e3b0a9e033362
bluelovers <codelovers@users.sourceforge.net>
2015-02-13 16:28:11 +0800

    . (Step: 2)
    Step: 2

--- defaults/preferences/forcecors.js
+++ defaults/preferences/forcecors.js
@@ -1,1 +1,3 @@
 pref('forcecors.headers', 'Access-Control-Allow-Origin *|Access-Control-Allow-Methods POST,GET');
+pref('extensions.forcecors.headers', 'Access-Control-Allow-Origin *|Access-Control-Allow-Methods POST,GET');
+pref('extensions.forcecors.enabled', false);
